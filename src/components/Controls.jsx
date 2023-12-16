37fa8e87ea9d54e2374069304948df9279b840c5
shubhamyadav97660@gmail.com <neeta6854@gmail.com>
2023-12-16 20:45:28 +0530

    first commit

---
+++ src/components/Controls.jsx
@@ -0,0 +1,92 @@
+import { useDispatch } from "react-redux";
+import { useRef } from "react";
+
+const Controls = () => {
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
+  const handleDecrement = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+
+  const handleAdd = () => {
+    dispatch({
+      type: "ADD",
+      payload: {
+        num: inputElement.current.value,
+      },
+    });
+    inputElement.current.value = "";
+  };
+
+  const handleSubtract = () => {
+    dispatch({
+      type: "SUBTRACT",
+      payload: {
+        num: inputElement.current.value,
+      },
+    });
+    inputElement.current.value = "";
+  };
+
+  const handlePrivacyToggle = () => {
+    dispatch({
+      type: "PRIVACY_TOGGLE",
+    });
+  };
+
+  const inputElement = useRef();
+
+  return (
+    <>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={handleIncrement}
+        >
+          +1
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={handleDecrement}
+        >
+          -1
+        </button>
+        <button
+          type="button"
+          className="btn btn-warning"
+          onClick={handlePrivacyToggle}
+        >
+          Privacy Toggle
+        </button>
+      </div>
+
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center control-row">
+        <input
+          type="text"
+          placeholder="Enter number"
+          className="number-input"
+          ref={inputElement}
+        ></input>
+        <button type="button" className="btn btn-info" onClick={handleAdd}>
+          Add
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={handleSubtract}
+        >
+          Subtract
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Controls;
